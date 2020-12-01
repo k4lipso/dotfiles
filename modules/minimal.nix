95e9ba400c0fb6ce8946dda4d90a22240bf0e35b
kalipso <kalipso@c3d2.de>
2020-12-01 20:47:00 +0100

    split general.nix into minimal and general

---
+++ modules/minimal.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    git
+    htop
+    bmon
+    wavemon
+    emacs
+    tree
+    gnupg
+    emacs
+    gcc
+    cmake
+    zsh
+    oh-my-zsh
+    rsync
+    pass
+    nmap
+    killall
+    direnv
+    youtube-dl
+    unzip
+    zip
+  ];
+
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "man" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.gnupg.agent = { enable = true; };
+
+  #TimeZone
+  time.timeZone = "Europe/Berlin";
+}
