193f147836ffc34883d39311475312742289ebb7
kalipso <kalipso@c3d2.de>
2021-01-05 14:58:39 +0100

    add minimal gtw configuration

--- machines/gtw/configuration.nix
+++ machines/gtw/configuration.nix
@@ -1,101 +1,89 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+ Keys = import ../../ssh_keys.nix;
+in
 {
   imports =
     [ # Include the results of the hardware scan.
-      #<nixos-hardware/lenovo/thinkpad/t480s>
       ./hardware-configuration.nix
+      ../../modules/home-manager.nix
+      ../../modules/minimal.nix
     ];
 
-  systemd.services.systemd-user-sessions.enable = false;
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-  environment.systemPackages = with pkgs; [
-    wget
-    vim
-    git
-    htop
-    bmon
-    wavemon
-    rxvt_unicode
-    acpi
-    tree
-    gnupg
-    emacs
-    gcc
-    cmake
-    iw
-    zsh
-    oh-my-zsh
-    rsync
-    pass
-    nmap
-    mpv
-    killall
-    direnv
-    youtube-dl
-    unzip
-    zip
-  ];
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "/dev/disk/by-id/ata-ST3500418AS_9VMSREAL";
+  #boot.supportedFilesystems = [ "zfs" ];
+  #boot.loader.grub.version = 2;
+  #boot.zfs.requestEncryptionCredentials = true;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking = {
-    hostName = "gtw1"; # Define your hostname.
-    hostId = "13377331";
-
-    interfaces = {
-      eth0 = {
-        name = "et0";
-        useDHCP = false;
-      };
-
-      eth1 = {
-        name = "eth1";
-        ipv4.addresses = [ {
-          address = "10.10.1.1";
-          prefixLength = 24;
-        } ];
-        useDHCP = true;
-      };
+  boot.initrd.network = {
+    enable = true;
+    ssh = {
+      enable = true;
+      # Use a different port than your usual SSH port!
+      port = 2233;
+      hostKeys = [ "/etc/secrets/initrd/ssh_host_rsa_key" "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+      # All users being a member of the "wheel" group are allowed to connect and enter the password.
+      #authorizedKeys = with lib; concatLists (mapAttrsToList (name: user: if elem "wheel" user.extraGroups then user.openssh.authorizedKeys.keys else []) config.users.users);
+      authorizedKeys = Keys.Kalipso;
     };
+    postCommands = ''
+      echo "zfs load-key -a; killall zfs" >> /root/.profile
+    '';
   };
 
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+  networking.hostId = "8f5bb625";
+  networking.dhcpcd.enable = true;
+  networking.usePredictableInterfaceNames = false;
+  networking.hostName = "gtw";
+
+  #systemd.network = {
+  #  enable = true;
+  #  networks."eth0".extraConfig = import ./network_config.nix;
+  #};
+
+  #networking.dhcpcd.enable = false;
+
+  services.openssh.enable = true;
+  services.openssh.ports = [ 22 ];
+  users.users.root.openssh.authorizedKeys.keys = Keys.Kalipso;
+  services.openssh.passwordAuthentication = false;
 
   users.users.kalipso = {
     isNormalUser = true;
-    home = "/home/kalipso";
-    initialPassword = "kalipso";
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = Keys.Kalipso;
     shell = pkgs.zsh;
   };
 
-  programs.zsh = {
-    enable = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  #networking.useDHCP = false;
+  #networking.interfaces.enp1s0.useDHCP = true;
+  #networking.interfaces.enp2s0.useDHCP = true;
 
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "man" ];
-      theme = "robbyrussell";
-    };
-  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
 }
+
