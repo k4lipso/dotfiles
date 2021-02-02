5f7ac0078385be1a51cae55dcca2e2c10693ba46
kalipso <kalipso@c3d2.de>
2021-02-02 14:56:16 +0100

    add flake support for ltpt

--- flake.nix
+++ flake.nix
@@ -2,23 +2,45 @@
   description = "NixOps Environments";
 
   inputs.utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
 
-  outputs = { self, nixpkgs, utils }:
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-  utils.lib.eachSystem [ "x86_64-linux" ]
-    (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+
+    inputs = {
+      nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, utils, nixos-hardware, home-manager, nixpkgs-stable, nixpkgs-unstable }:
+
+  let
+    pkgs-stable = nixpkgs-stable.legacyPackages.x86_64-linux;
+  in
   {
-    devShell = pkgs.stdenv.mkDerivation
+    devShell.x86_64-linux = pkgs-stable.stdenv.mkDerivation
       {
         name = "nixops-20_09-env";
-        NIX_PATH="nixpkgs=${pkgs.path}";
-        buildInputs = [ pkgs.nixops ];
+        NIX_PATH="nixpkgs=${pkgs-stable.path}";
+        buildInputs = [ pkgs-stable.nixops ];
       };
 
-  }
-  );
+    nixosConfigurations.nixos = nixpkgs-unstable.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+        ./machines/ltpt/configuration.nix
+        home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalipso = import ./modules/kalipso.nix;
+        }
+      ];
+    };
+
+  };
 }
