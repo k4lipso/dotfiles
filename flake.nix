87301e43c7c0348c011ab889ba4f57280b149215
kalipso <kalipso@c3d2.de>
2022-09-20 18:37:22 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
 {
   description = "NixOps Environments";
 
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    mfsync.url = "github:k4lipso/mfsync";
+    deploy.url = "github:input-output-hk/deploy-rs";
+    deploy.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
 
-  inputs.home-manager = {
-    url = "github:nix-community/home-manager";
-
-    inputs = {
-      nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+      };
     };
   };
-
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  inputs.sops-nix.url = "github:Mic92/sops-nix";
-
-  inputs.mfsync.url = "github:k4lipso/mfsync";
 
   outputs = { ... } @ args: import ./outputs.nix args;
 }
