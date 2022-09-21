770309482c66c090f67ff1b32a99c0340a0402b0
kalipso <kalipso@c3d2.de>
2022-09-21 13:57:48 +0200

    add checks and remove system.i686-linux

--- outputs.nix
+++ outputs.nix
@@ -7,7 +7,9 @@
 , ...
 } @inputs:
 
-(utils.lib.eachDefaultSystem ( system:
+# filter i686-liux from defaultSystem to run nix flake check successfully
+let filter_system = name: if name == utils.lib.system.i686-linux then false else true;
+in (utils.lib.eachSystem (builtins.filter filter_system utils.lib.defaultSystems) ( system:
   let
     pkgs-stable = nixpkgs-stable.legacyPackages."${system}";
     pkgs-unstable = nixpkgs-unstable.legacyPackages."${system}";
@@ -26,4 +28,6 @@
     deploy = import ./machines/deploy.nix (inputs // {
       inherit inputs;
     });
+
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy.lib;
 }
