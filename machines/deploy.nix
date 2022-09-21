574f2815d027879d7a33443837a46a63148274c9
kalipso <kalipso@c3d2.de>
2022-09-21 15:58:30 +0200

    add sshOpts to deploy

--- machines/deploy.nix
+++ machines/deploy.nix
@@ -5,6 +5,7 @@
 let
   mkNode = server: ip: fast: {
     hostname = "${ip}:2222";
+    sshOpts = [ "-p" "2222" ];
     fastConnection = fast;
     profiles.system.path =
       deploy.lib.x86_64-linux.activate.nixos
